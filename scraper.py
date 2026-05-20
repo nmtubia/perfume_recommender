c0f2687f451ea261ceb06057484d756b9e853c67
nmtubia <nmtubia@wm.edu>
2026-05-20 16:49:15 -0400

    headless browser setup, scrape_designers created

--- scraper.py
+++ scraper.py
@@ -5,29 +5,79 @@ import string
 import time
 import math
 from urllib.parse import urljoin
+from playwright.sync_api import sync_playwright
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.62 Safari/537.36'
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Connection": "keep-alive",
 }
-
 
 BASE = 'https://www.fragrantica.com'
 
 MAX_PER_DESIGNER = 10
 REQUEST_DELAY = 2.5
 
+def fetch(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        page.goto(url, wait_until="load", timeout=60000)
+
+        # safe cookie handling
+        try:
+            page.locator("text=Accept").click(timeout=3000)
+        except:
+            pass
+
+        page.wait_for_timeout(3000)
+
+        # optional scroll (keep for now)
+        page.mouse.wheel(0, 2000)
+        page.wait_for_timeout(3000)
+
+        html = page.content()
+
+        browser.close()
+        return html
+
+        # print("=== HTML SNAPSHOT ===")
+        # print(html[:1500])  # inspect what you're actually getting
+
 def get_designers(designer_index_url):
-    response = requests.get(designer_index_url, headers=headers)
+    html = fetch(designer_index_url)
+    soup = BeautifulSoup(html, "html.parser")
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-    else:
-        print(f"Failed to retrieve page, status code: {response.status_code}")
-
-    elements = soup.find_all('div', class_="designerlist cell small-6 large-4")
+    elements = soup.select("div.h-full a[href^='/designers/']")
 
     designers = {}
 
-    for element in elements:
-        
+    for a in elements:
+        name = a.get_text(strip=True)
+        href = a.get("href")
+
+        if not href or not name:
+            continue
+
+        url = urljoin(BASE, href)
+
+        designers[name] = url
+
+    return designers
+    
+    #def get_perfumes(designer_name, designer_url):
+
+url = "https://www.fragrantica.com/designers/"
+
+designers = get_designers(url)
+
+print(len(designers))
+print(list(designers.items())[:10])
+
+
+
+
+
 
